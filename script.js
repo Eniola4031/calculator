37065f25878a5852ca8397baf853c4aa2b576d27
eniola4031 <agboola.eniola4031@gmail.com>
2021-04-17 15:35:10 +0100

    fixes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //create an object to help us keep track of values
 const calculator = {
     displayValue: '0',
@@ -6,11 +7,46 @@ const calculator = {
     operator: null,
   };
   function updateDisplay() {
-    // select the element with class of `calculator-screen`
-    const display = document.querySelector('.calculator-screen');
+    // select the element with class of `display`
+    const display = document.querySelector('.calc-screen');
     // update the value of the element with the contents of `displayValue`
     display.value = calculator.displayValue;
   }
-  
   updateDisplay();
   
+  const keys = document.querySelector('.calc-screen');
+keys.addEventListener('click', (event) => {
+  // Access the clicked element
+  const { target } = event;
+
+  // Check if the clicked element is a button.
+  // If not, exit from the function
+  if (!target.matches('div')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+  if (target.classList.contains('function')) {
+    console.log('function', target.value);
+    return;
+  }
+  if (target.classList.contains('number')) {
+    console.log('number', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  console.log('digit', target.value);
+});
